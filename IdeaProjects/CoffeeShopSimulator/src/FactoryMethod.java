cd561b98f487ef9b8995d85a8d386d52462ff12d
Makegoiii <magzhankydyrbai05@gmail.com>
2025-03-16 16:37:36 +0500

    Dobavil faily CoffeeShop

---
+++ IdeaProjects/CoffeeShopSimulator/src/FactoryMethod.java
@@ -0,0 +1,11 @@
+class CoffeeFactory {
+    public static Coffee createCoffee(String type) {
+        return switch (type.toLowerCase()) {
+            case "espresso" -> new Espresso();
+            case "cappuccino" -> new Cappuccino();
+            case "latte" -> new Latte();
+            case "americano" -> new Americano();
+            default -> throw new IllegalArgumentException("Invalid coffee type");
+        };
+    }
+}
