243d2705746567e714ced01af79b7571188b25c8
Makegoiii <magzhankydyrbai05@gmail.com>
2025-05-12 00:20:26 +0500

    added files

--- IdeaProjects/FlyWeightHomework5/src/FlyWeight/Command.java
+++
@@ -1,6 +0,0 @@
-package FlyWeight;
-public interface Command {
-    void execute();
-    void undo();
-}
-
--- IdeaProjects/ProxyPatternHomework5/src/Proxy/UploadImageHandler.java
+++
@@ -1,9 +0,0 @@
-package Proxy;
-public class UploadImageHandler extends ImageUploader {
-    @Override
-    public boolean handle(String user, String filename) {
-        System.out.println("Изображение" + filename + " успешно загружено!");
-        return true;
-    }
-}
-
